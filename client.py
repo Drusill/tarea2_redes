ddfde1d5430844b25d39b8836a5117cf8c255511
drusill <raul.cid@ing.uchile.cl>
2018-11-28 08:50:31 -0300

    Intento de establecer conexion, three way hanshake

--- client.py
+++ client.py
@@ -1,4 +1,61 @@
 # -*- coding: utf-8 -*-
 import socket
 import sys
+import random
+import os
 
+if len(sys.argv) != 4:
+    print "python server.py IPaddress PortNumber Filename"
+    sys.exit()
+
+# Socket
+connection = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+# Parámetros
+ip_Server = sys.argv[1]
+port_Server = int(sys.argv[2])
+filename = sys.argv[3]
+size_file = os.path.getsize(filename)
+address = (ip_Server, port_Server)
+
+# Ventana y tamaño de paquete
+buff = 1024
+window_Size = 10
+window = []
+seq_max = 2*window_Size 
+"""
+i = 0
+while i < window_Size:
+    window[i] = i
+    i += 1
+"""
+
+
+# Header
+seq_number = random.randint(0, seq_max)
+SYN = 1
+data = str(filename) + "|||" + str(size_file) + "|||" + str(SYN) +
+         "|||" + str(seq_max) + "|||" + str(seq_number) 
+
+
+# file_toSend = open(filename, "rb")
+
+try_connection = 0
+while True:
+    connection.sendto(data, address)
+    seq_number = (seq_number + 1) % seq_max
+    connection.settimeout(0.5)
+    try:
+        if try_connection == 5:
+            print "Cannot establish connection"
+            break
+
+        data, adress = connection.recvfrom(buff)
+
+        if not data:
+            break
+
+        (SYN, ACK_Flag, ACK, seq_number) = data.split("|||")
+        if str(SYN) == "1" and STR(ACK_Flag) == str(seq_number) and str(seq_number) = str(ACK):
+            file_toSend = open(filename, "rb")
+            data = 
--- server.py
+++ server.py
@@ -1,28 +1,38 @@
 # -*- coding: utf-8 -*-
 import socket
 import sys
+import random
 import os
 
-if len(sys.argv) != 4:
-    print "python server.py IPaddress PortNumber Filename"
+if len(sys.argv) != 2:
+    print "python server.py PortNumber"
     sys.exit()
 
-# Parámetros
-ip_Server = sys.argv[1]
-port_Server = int(sys.argv[2])
-filename = sys.argv[3]
+# Socket
+ip = ""
+port = int(sys.argv[1])
+connection = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+connection.bind((ip, port))
 
-# Ventana y tamaño de paquete
+# Parámetros
 buff = 1024
-window_Size = 100
-window = []
-"""
-i = 0
-while i < window_Size:
-    window[i] = i
-    i += 1
-"""
-# Header
-seq_number = 0
-ACK_Flag = 0
-header = str(seq_number) + "|||" + str(ACK_Flag)
+ACK = 0
+seq_number_sv = random.randint(0, 100)
+current_size = 0
+percent = round(0,2)
+can_recieve = True
+
+while True:
+    packet, address = connection.recvfrom(buff)
+    if packet:
+        (filename, size_file, SYN, seq_max, seq_number_cl) = packet.split("|||")
+
+        if str(SYS) == "1":
+            sended_file = open("file_"+filename, "wb", )
+            print("Connecting, waiting for response")
+            ACK_Flag = 1
+            ACK = (seq_number_cl + 1) % seq_max
+            data = str(SYN) + "|||" + str(ACK_Flag) 
+                    + "|||" + str(ACK) + "|||" + str(seq_number_sv)
+                
+            connection.sendto(data, address)
